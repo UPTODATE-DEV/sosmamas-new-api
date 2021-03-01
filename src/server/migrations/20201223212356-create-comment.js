2f6c44d7b9cfb67ca853fcb8674809508bcdac35
baruka99 <baruka99.david@gmail.com>
2021-03-01 23:49:16 +0200

    Fix user Fk model in migration

--- src/server/migrations/20201223212356-create-comment.js
+++ src/server/migrations/20201223212356-create-comment.js
@@ -15,7 +15,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
         references: {
-          model: 'users', // name of Target model
+          model: 'Users', // name of Target model
           key: 'id', // key in Target model that we're referencing
         },
       },
